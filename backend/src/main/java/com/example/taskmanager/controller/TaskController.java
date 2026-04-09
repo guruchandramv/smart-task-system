7cbb1346c21b286cdaccf2a1a8093bd569b7f61c
guruchandramv <guruchandra104@gmail.com>
2026-04-09 15:20:22 +0530

    Fixed DB Values in TaskController

--- backend/src/main/java/com/example/taskmanager/controller/TaskController.java
+++ backend/src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -57,9 +57,9 @@ public class TaskController {
     public String testDatabase() {
         try {
             Class.forName("org.postgresql.Driver");
-            String url = System.getenv("jdbc:postgresql://ep-noisy-fog-a1chnaai-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require");
-            String user = System.getenv("neondb_owner");
-            String pass = System.getenv("npg_63RoatjmPysn");
+            String url =  "jdbc:postgresql://ep-noisy-fog-a1chnaai-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+            String user = "neondb_owner";
+            String pass = "npg_63RoatjmPysn";
 
             Connection conn = DriverManager.getConnection(url, user, pass);
             conn.close();
