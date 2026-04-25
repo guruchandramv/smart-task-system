f45c7b821945b446d030b50adee576817610d3a9
guruchandramv <guruchandra104@gmail.com>
2026-04-25 18:07:43 +0530

    Fixed Frontend Resource file to manually switch from local/production

--- frontend-react-app/src/axiosConfig.js
+++ frontend-react-app/src/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL = "https://smart-task-system-wz0k.onrender.com";
+export const API_URL = "http://localhost:8080";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
